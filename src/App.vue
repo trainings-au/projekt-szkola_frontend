dcfd38971806247f8c83833d524f3cf691aa6409
trainings-au <85095377+trainings-au@users.noreply.github.com>
2021-05-31 10:34:22 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    Czy nowe rzeczy są na githubie ?
+    Czy nowe rzeczy są na githubie ? Zobaczymy
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
